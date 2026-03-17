a4a8a33b2ae859ee8db4d3c3fee896868b2a1ad5
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2026-03-17 18:43:27 +0900

    style: ErrorBoundary type-only import 적용

--- src/components/ErrorBoundary/index.tsx
+++ src/components/ErrorBoundary/index.tsx
@@ -1,5 +1,5 @@
 import { Component, type ErrorInfo, type ReactNode } from 'react';
-import { ErrorBoundaryProps, ErrorBoundaryState } from './types';
+import type { ErrorBoundaryProps, ErrorBoundaryState } from './types';
 
 export class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
   constructor(props: ErrorBoundaryProps) {
