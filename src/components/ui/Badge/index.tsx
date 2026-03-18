7d13a499d4ddbc83bfb586e42aa6871256d09884
gajaeup <jenny436543@gmail.com>
2026-03-18 11:06:19 +0900

    feat: 공통 컴포넌트 Badge 추가(SM-34)

---
+++ src/components/ui/Badge/index.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { cva, type VariantProps } from 'class-variance-authority';
+import { type HTMLAttributes, type ReactNode } from 'react';
+import { cn } from '@/lib/cn';
+
+const badgeVariants = cva('', {
+  variants: {
+    variant: {
+      category: '',
+      status: '',
+      tag: '',
+    },
+  },
+  defaultVariants: {
+    variant: 'tag',
+  },
+});
+
+interface BadgeProps extends HTMLAttributes<HTMLSpanElement>, VariantProps<typeof badgeVariants> {
+  children: ReactNode;
+}
+
+export function Badge({ children, variant, className, ...props }: BadgeProps) {
+  return (
+    <span className={cn(badgeVariants({ variant }), className)} {...props}>
+      {children}
+    </span>
+  );
+}
