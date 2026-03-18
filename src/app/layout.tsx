d9410960658a2b645d5850165b5608464ab76716
Ahyeon Choi <138025938+strawberryroll@users.noreply.github.com>
2026-03-18 10:16:51 +0900

    [SM-32] feat: TanStack Query 초기 설정 및 QueryClient 구조 구성 (#30)
    * feat: TanStack Query 초기 설정 및 QueryClient 구조 구성
    
    * chore: QueryClient 관련 코드 컨벤션 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import { QueryProvider } from '@/providers/QueryProvider';
 
 export default function RootLayout({
   children,
@@ -7,7 +8,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='ko'>
-      <body>{children}</body>
+      <body>
+        <QueryProvider>{children}</QueryProvider>
+      </body>
     </html>
   );
 }
