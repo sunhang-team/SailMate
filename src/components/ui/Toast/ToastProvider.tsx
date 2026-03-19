ee32dd8326a8aca02e05b9e01b53c168ef8e1006
gajaeup <jenny436543@gmail.com>
2026-03-19 10:40:34 +0900

    refactor:z-index 스케일 변경(SM-38)

--- src/components/ui/Toast/ToastProvider.tsx
+++ src/components/ui/Toast/ToastProvider.tsx
@@ -22,7 +22,7 @@ export function ToastProvider({ children }: ToastProviderProps) {
       {children}
       {portalRoot &&
         createPortal(
-          <div className={cn('pointer-events-none fixed top-4 right-4 z-9999 flex flex-col gap-2')}>
+          <div className={cn('pointer-events-none fixed top-4 right-4 z-50 flex flex-col gap-2')}>
             {toasts.map((toast) => (
               <div key={toast.id} className='pointer-events-auto'>
                 <Toast
