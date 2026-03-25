eed895d10f3d23ed323aa6a78d8ef6bcf5d12b69
kwonjin2 <rnjswls060@gmail.com>
2026-03-25 09:22:54 +0900

    [SM-61] feat: Applications 도메인 API 함수 및 쿼리 훅, MSW 모킹 구현 (#87)
    * feat(api): Applications(모임 신청) 도메인 API 함수 초기 세팅
    
    - 모임 참여 신청 및 취소 (createApplication, deleteApplication)
    - 모임장용 신청 목록 조회 및 상태 변경 (getApplicationList, updateApplicationStatus)
    - 내 신청 목록 조회 (getMyApplicationList)
    - 기존에 정의된 types를 반환 타입으로 매핑 완료
    
    * feat: 모임 신청(applications) TanStack Query 훅 구현
    
    - 팩토리 패턴 기반의 쿼리 키 정의
    - GET 요청에 queryOptions 패턴을 적용하여 prefetch 대응
    - Mutation 훅 내 외부 options 주입 로직 구현
    
    * feat: 모임 신청(Applications) MSW 핸들러 추가
    
    - 모임 신청 관련 API의 MSW 모킹 핸들러 구현
      - POST /api/v1/gaterings/:gatheringId/applications 모임 참여 신청
      - GET /api/v1/gatherings/:gatheringId/applications 신청 목록 조회(모임장)
      - PATCH /api/v1/gatherings/:gatheringId/applications/:applicationId 신청 수락/거절(모임장)
      - DELETE /api/v1/gatherings/:gatheringId/applications/:applicationId 신청 취소
      - GET /api/v1/users/me/applications 내 신청 목록 조회
    - 구현한 applicationsHandlers를 MSW 글로벌 핸들러에 등록
    
    * fix: mutation훅에서 invalidateQueries시 all키로 무효화

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -2,5 +2,6 @@ import { todosHandlers } from './todos';
 import { authHandlers } from './auth';
 import { usersHandlers } from './users';
 import { membershipsHandlers } from './memberships';
+import { applicationsHandlers } from './applications';
 
-export const handlers = [...todosHandlers, ...authHandlers, ...usersHandlers, ...membershipsHandlers];
+export const handlers = [...todosHandlers, ...authHandlers, ...usersHandlers, ...membershipsHandlers, ...applicationsHandlers];
