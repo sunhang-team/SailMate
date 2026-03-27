ad1a050c0c7b7056656abaffc5ae418aecc268ea
Ahyeon Choi <138025938+strawberryroll@users.noreply.github.com>
2026-03-27 15:49:57 +0900

    [SM-79] feat: 로그인 페이지 구현 (OAuth 미연동) (#117)
    * feat: 로그인 페이지 구현
    
    * refactor: 로그인 페이지 구조를 회원가입 페이지와 통일
    
    * fix: 로그인 성공 후 리다이렉트 경로를 /main으로 수정

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -5,7 +5,7 @@ import { EmailLoginForm } from './EmailLoginForm';
 
 export default function LoginPage() {
   return (
-    <main className='flex justify-center px-4 py-20 md:py-25'>
+    <main className='flex justify-center px-4 py-20'>
       <div className='w-full max-w-[496px]'>
         <h1 className='text-h5-b mb-10 text-center text-gray-900'>로그인</h1>
         <SocialLoginButtons kakaoLabel='카카오로 로그인' googleLabel='구글로 로그인' />
