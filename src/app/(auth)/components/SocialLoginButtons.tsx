4bc3fc42ad5854a7ae1d8144107779bfb570cd20
ahyeon <nuyha234@gmail.com>
2026-03-29 00:21:55 +0900

    style: 로그인 페이지 반응형 레이아웃 적용

--- src/app/(auth)/components/SocialLoginButtons.tsx
+++ src/app/(auth)/components/SocialLoginButtons.tsx
@@ -8,12 +8,12 @@ interface SocialLoginButtonsProps {
 
 export function SocialLoginButtons({ kakaoLabel, googleLabel }: SocialLoginButtonsProps) {
   return (
-    <div className='mb-10 flex gap-3'>
-      <Button variant='social-kakao' size='social-kakao' className='flex-1'>
+    <div className='mb-10 flex flex-col gap-3 md:flex-row'>
+      <Button variant='social-kakao' size='social-kakao' className='w-full md:flex-1'>
         <KakaoIcon size={20} />
         {kakaoLabel}
       </Button>
-      <Button variant='social' size='social' className='flex-1'>
+      <Button variant='social' size='social' className='w-full md:flex-1'>
         <GoogleIcon size={20} />
         {googleLabel}
       </Button>
