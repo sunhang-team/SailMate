cc55381257a883575e605c638ad1e7c80b410817
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2026-03-31 11:08:43 +0900

    feat: 모임 목록 카드 그리드 및 페이지네이션 구현

---
+++ src/components/Search/GatheringList/Skeleton.tsx
@@ -0,0 +1,17 @@
+const SKELETON_COUNT = 8;
+
+export function GatheringListSkeleton() {
+  return (
+    <>
+      <div className='flex flex-col gap-4'>
+        <div className='h-7 w-32 animate-pulse rounded bg-gray-200' />
+        <div className='h-5 w-48 animate-pulse rounded bg-gray-200' />
+      </div>
+      <div className='grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+        {Array.from({ length: SKELETON_COUNT }).map((_, i) => (
+          <div key={i} className='h-72 animate-pulse rounded-xl bg-gray-200' />
+        ))}
+      </div>
+    </>
+  );
+}
