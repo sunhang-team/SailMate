411520c73c9569d9475c35a447a5ad3abd39cae9
kwonjin2 <rnjswls060@gmail.com>
2026-03-31 17:32:56 +0900

    feat: 메인 화면 페이지 통합 및 캐시 동기화(Hydration) 환경 구축
    - 서버 컴포넌트 환경에서 queryClient.prefetchQuery를 통해 메인 페이지 진입 시 필요한 초기 모임 데이터 확보(8개)
    - 확보된 상태를 HydrationBoundary로 내려주어, 클라이언트의 TanStack Query 캐시와 동기화 작업
    - revalidate 옵션을 캐시 정책에 맞게 3600으로 설정하여 ISR 적용

--- src/app/main/constant/constant.ts
+++ src/app/main/constant/constant.ts
@@ -1,2 +1,3 @@
 export const MAX_GATHERING_LIMIT = 8;
 export const FIRST_PAGE = 1;
+export const MAIN_PAGE_REVALIDATE_TIME = 3600;
--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,7 +1,31 @@
-export default function MainPage() {
+import { getQueryClient } from '@/lib/getQueryClient';
+import { DeadlineGatheringSection } from './components/DeadlineGatheringSection';
+import { HeroSection } from './components/HeroSection';
+import { LatestGatheringSection } from './components/LatestGatheringSection';
+import { MyGatheringSection } from './components/MyGatheringSection';
+import { PopularGatheringSection } from './components/PopularGatheringSection';
+import { gatheringQueries } from '@/api/gatherings/queries';
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import { MAX_GATHERING_LIMIT, MAIN_PAGE_REVALIDATE_TIME } from './constant/constant';
+
+export const revalidate = MAIN_PAGE_REVALIDATE_TIME;
+
+export default async function MainPage() {
+  const queryClient = getQueryClient();
+
+  await queryClient.prefetchQuery(gatheringQueries.main({ limit: MAX_GATHERING_LIMIT }));
+
   return (
-    <div>
-      <h1>Main Page</h1>
-    </div>
+    <>
+      <HeroSection />
+      <div className='mx-auto flex w-full max-w-[1920px] flex-col gap-15 px-4 py-10 md:gap-26 md:px-7 md:py-20 lg:gap-30 lg:px-12 lg:py-24 xl:px-20 2xl:px-30'>
+        <MyGatheringSection />
+        <HydrationBoundary state={dehydrate(queryClient)}>
+          <PopularGatheringSection />
+          <DeadlineGatheringSection />
+          <LatestGatheringSection />
+        </HydrationBoundary>
+      </div>
+    </>
   );
 }
