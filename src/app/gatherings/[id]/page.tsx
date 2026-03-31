497c63813cbc29b1eb49271fafa0fa8b515a1e54
rak517 <166088638+rak517@users.noreply.github.com>
2026-03-31 18:56:26 +0900

    [SM-96] feat: 모임 상세 페이지 라우트, 레이아웃, 히어로 섹션 구현 (#142)
    * style: text-small-02-sb 텍스트 스타일 추가
    
    * feat: 모임 상세 queryFn 서버/클라이언트 분기 처리
    
    * chore: 모임 상세 목 데이터 확장
    
    * feat: 모임 상세 페이지 라우트 및 히어로 섹션 구현
    
    * style: 모임 목록 페이지 min-h-screen 적용
    
    * docs: 주석추가

--- src/app/gatherings/[id]/page.tsx
+++ src/app/gatherings/[id]/page.tsx
@@ -4,12 +4,7 @@ import { ErrorBoundary } from '@/components/ErrorBoundary';
 import { getQueryClient } from '@/lib/getQueryClient';
 import { gatheringQueries } from '@/api/gatherings/queries';
 
-import { AnchorTabNav } from './_components/AnchorTabNav';
-import { GatheringDetailContent } from './_components/GatheringDetailContent';
-import { FloatingActionBar } from './_components/ApplySection/FloatingActionBar';
 import { GatheringHero } from './_components/GatheringHero';
-import { GatheringInfoAside } from './_components/ApplySection/GatheringInfoAside';
-import { GatheringInfoCard } from './_components/ApplySection/GatheringInfoCard';
 
 interface GatheringDetailPageProps {
   params: Promise<{ id: string }>;
@@ -24,35 +19,25 @@ export default async function GatheringDetailPage({ params }: GatheringDetailPag
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <main className='mb-20 min-h-screen'>
+      <main className='min-h-screen'>
         <ErrorBoundary
           fallback={<p className='py-20 text-center text-gray-500'>모임 정보를 불러오는데 실패했습니다.</p>}
         >
           <GatheringHero gatheringId={gatheringId} />
 
           {/* TODO: [이슈 2] Mobile/Tablet 사이드바 정보 카드 (xl:hidden) */}
-
-          {/* Mobile/Tablet: 모임 정보 카드 (탭 위에 배치) */}
-          <div className='px-4 pt-6 md:px-8 xl:hidden'>
-            <GatheringInfoCard gatheringId={gatheringId} />
-          </div>
-
           {/* TODO: [이슈 3] 앵커 탭 네비게이션 */}
 
-          <AnchorTabNav />
-
-          <div className='px-4 pt-10 md:px-7 xl:flex xl:gap-20 xl:px-30'>
+          <div className='mx-auto max-w-[1680px] px-4 md:px-7 xl:flex xl:gap-20 xl:px-0'>
             <section className='min-w-0 flex-1'>
-              <GatheringDetailContent gatheringId={gatheringId} />
+              {/* TODO: [이슈 3] 모임 설명, 이미지 캐러셀, 모집/활동 기간, 최종 목표 */}
+              {/* TODO: [이슈 4] 주차별 계획 아코디언, 모집 인원 현황 */}
             </section>
 
-            {/* Desktop: 사이드바 (sticky) */}
             <aside className='hidden xl:block xl:w-[560px] xl:shrink-0'>
-              <GatheringInfoAside gatheringId={gatheringId} />
+              {/* TODO: [이슈 2] Desktop 사이드바 (sticky) */}
             </aside>
           </div>
-          {/* Mobile/Tablet: 하단 고정 액션 바 */}
-          <FloatingActionBar gatheringId={gatheringId} />
         </ErrorBoundary>
       </main>
     </HydrationBoundary>
