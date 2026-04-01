ae9d40d667331c42911cff25bd8c6c845a47dd17
kwonjin2 <rnjswls060@gmail.com>
2026-04-01 11:14:27 +0900

    feat: 상세페이지에 ApplySection 적용
    - xl이상일 땐 GatheringInfoCard 안보이게
    - xl에서만 GatheringInfoAside 보이게
    - FloatingActionBar는 에러바운더리 내부로 배치

--- src/app/gatherings/[id]/page.tsx
+++ src/app/gatherings/[id]/page.tsx
@@ -6,7 +6,10 @@ import { gatheringQueries } from '@/api/gatherings/queries';
 
 import { AnchorTabNav } from './_components/AnchorTabNav';
 import { GatheringDetailContent } from './_components/GatheringDetailContent';
+import { FloatingActionBar } from './_components/ApplySection/FloatingActionBar';
 import { GatheringHero } from './_components/GatheringHero';
+import { GatheringInfoAside } from './_components/ApplySection/GatheringInfoAside';
+import { GatheringInfoCard } from './_components/ApplySection/GatheringInfoCard';
 
 interface GatheringDetailPageProps {
   params: Promise<{ id: string }>;
@@ -21,13 +24,20 @@ export default async function GatheringDetailPage({ params }: GatheringDetailPag
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <main className='min-h-screen'>
+      <main className='min-h-screen pb-20 xl:pb-0'>
         <ErrorBoundary
           fallback={<p className='py-20 text-center text-gray-500'>모임 정보를 불러오는데 실패했습니다.</p>}
         >
           <GatheringHero gatheringId={gatheringId} />
 
           {/* TODO: [이슈 2] Mobile/Tablet 사이드바 정보 카드 (xl:hidden) */}
+
+          {/* Mobile/Tablet: 모임 정보 카드 (탭 위에 배치) */}
+          <div className='px-4 pt-6 md:px-8 xl:hidden'>
+            <GatheringInfoCard gatheringId={gatheringId} />
+          </div>
+
+          {/* TODO: [이슈 3] 앵커 탭 네비게이션 */}
 
           <AnchorTabNav />
 
@@ -37,10 +47,13 @@ export default async function GatheringDetailPage({ params }: GatheringDetailPag
               {/* TODO: [이슈 4] 주차별 계획 아코디언, 모집 인원 현황 */}
             </section>
 
-            <aside className='xl:block xl:w-[560px] xl:shrink-0'>
-              {/* TODO: [이슈 2] Desktop 사이드바 (sticky) */}
+            {/* Desktop: 사이드바 (sticky) */}
+            <aside className='hidden xl:block xl:w-[560px] xl:shrink-0'>
+              <GatheringInfoAside gatheringId={gatheringId} />
             </aside>
           </div>
+          {/* Mobile/Tablet: 하단 고정 액션 바 */}
+          <FloatingActionBar />
         </ErrorBoundary>
       </main>
     </HydrationBoundary>
