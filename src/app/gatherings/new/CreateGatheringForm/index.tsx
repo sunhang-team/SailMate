d16ea975ebde53fb64dff168e08fbb7e3e07d8d7
Ahyeon Choi <138025938+strawberryroll@users.noreply.github.com>
2026-04-01 17:49:40 +0900

    [SM-104] feat: 모임 만들기 페이지 이미지 업로드 섹션 구현 (#165)
    * feat: 모임 만들기 페이지 이미지 업로드 섹션 구현
    
    * feat: 이미지 1장 이상일 때 드래그앤드롭 지원 및 드롭존 flickering 수정
    
    * style: 반응형 스타일 적용
    
    * fix: flex 클래스 누락 수정

--- src/app/gatherings/new/CreateGatheringForm/index.tsx
+++ src/app/gatherings/new/CreateGatheringForm/index.tsx
@@ -18,6 +18,7 @@ import { gatheringFormPartialSchema } from '@/api/gatherings/schemas';
 import { GATHERING_CATEGORIES, GATHERING_TYPES } from '@/constants/gathering';
 import { cn } from '@/lib/cn';
 
+import { ImageUpload } from './ImageUpload';
 import { TagInput } from './TagInput';
 
 import type { GatheringForm, GatheringFormPartial } from '@/api/gatherings/types';
@@ -282,6 +283,18 @@ export function CreateGatheringForm() {
         />
       </div>
 
+      {/* 이미지 */}
+      <div className='flex flex-col gap-1'>
+        <p className='text-small-02-m md:text-body-02-m lg:text-body-01-m text-gray-800'>이미지</p>
+        <Controller
+          name='images'
+          control={control}
+          render={({ field }) => (
+            <ImageUpload value={field.value ?? []} onChange={field.onChange} error={errors.images?.message} />
+          )}
+        />
+      </div>
+
       {/* 모임 최종 목표 */}
       <div className='flex flex-col gap-1'>
         <Input
@@ -298,9 +311,6 @@ export function CreateGatheringForm() {
         />
         <p className='text-small-02-r self-end text-gray-400'>{goalValue.length}/200</p>
       </div>
-
-      {/* 이미지 */}
-      <div />
 
       {/* 날짜/일정 */}
       <div />
