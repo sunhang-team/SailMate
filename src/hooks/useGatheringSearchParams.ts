192f8005189846d13d896774705d6d4ef003400d
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2026-04-07 11:45:57 +0900

    refactor: categoryId 단일 값을 categoryIds 배열로 변경

--- src/hooks/useGatheringSearchParams.ts
+++ src/hooks/useGatheringSearchParams.ts
@@ -1,6 +1,12 @@
 'use client';
 
-import { useQueryParams, parseAsString, parseAsStringEnum, parseAsInteger } from '@frontend-toolkit-js/hooks';
+import {
+  useQueryParams,
+  parseAsString,
+  parseAsStringEnum,
+  parseAsInteger,
+  parseAsArray,
+} from '@frontend-toolkit-js/hooks';
 
 import { GATHERING_TYPES } from '@/constants/gathering';
 
@@ -11,7 +17,7 @@ export const useGatheringSearchParams = () => {
   return useQueryParams({
     query: parseAsString.withDefault(''),
     type: parseAsStringEnum(GATHERING_TYPES),
-    categoryId: parseAsInteger,
+    categoryIds: parseAsArray(parseAsInteger).withDefault([]),
     sort: parseAsStringEnum(SORT_OPTIONS).withDefault('latest'),
     status: parseAsStringEnum(STATUS_OPTIONS).withDefault('RECRUITING'),
     page: parseAsInteger.withDefault(1),
