105486773ce704a3e3f418b39aebd37ae99255a7
ahyeon <nuyha234@gmail.com>
2026-05-08 14:48:06 +0900

    feat: 서비스 소개, 이용 약관, 개인정보 처리방침 정적 페이지 구현

---
+++ src/app/(info)/layout.tsx
@@ -0,0 +1,3 @@
+export default function InfoLayout({ children }: { children: React.ReactNode }) {
+  return <main className='mx-auto max-w-[800px] px-4 py-10 md:px-6 md:py-12 xl:py-16'>{children}</main>;
+}
