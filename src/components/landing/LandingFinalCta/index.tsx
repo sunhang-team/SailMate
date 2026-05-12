a53d5a6f06cbd5d25609f569e9807c5f1efc0ba2
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2026-05-12 16:17:58 +0900

    style: 랜딩페이지 하단 CTA 텍스트 간격 조정

--- src/components/landing/LandingFinalCta/index.tsx
+++ src/components/landing/LandingFinalCta/index.tsx
@@ -21,7 +21,7 @@ export function LandingFinalCta() {
         <p className='text-small-01-b md:text-h5-b lg:text-h4-b text-blue-500'>
           더 나은 완성도를 위한 가장 확실한 선택
         </p>
-        <h2 className='text-body-01-b md:text-h3-b lg:text-h2-b mt-3 mb-7 text-blue-500'>
+        <h2 className='text-body-01-b md:text-h3-b lg:text-h2-b mt-1 mb-7 text-blue-500'>
           지금 <span className='text-gradient-primary'>완성도</span>에서{' '}
           <span className='text-gradient-primary'>항해</span>를 시작하세요.
         </h2>
